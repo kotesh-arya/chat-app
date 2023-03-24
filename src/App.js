8eef612d2e4f5abdba418fc5dd7ec3b13b18215c
kotesh-arya <koteswarraomudila@gmail.com>
2023-03-24 22:08:55 +0530

    FIX: Removed unused import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import MessageSection from './Components/MessageSection/MessageSection';
-import InputContainer from './Components/InputContainer/InputContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <MessageSection />
-      {/* <InputContainer /> */}
     </div>
   );
 }
